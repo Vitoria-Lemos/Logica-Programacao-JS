38c9c75773ca7e0dd739874a25f05006ed1d8259
Vitoria_lemos <73767279+Vitoria-Lemos@users.noreply.github.com>
2023-10-24 11:07:58 -0300

    Update 1002.js

--- 1002.js
+++ 1002.js
@@ -1,16 +1,3 @@
-const path = require('path');
-const currentFile = path.dirname(process.argv[1]);
-
-//realiza a leitura das linhas no arquivo dev\stdin
-var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
-var lines = input.split('\r\n');
-
-/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
-
-//quebrando a entrada de dados de uma linha em variaveis separadas
-//quando os valores estiverem na mesma linha
-//lines = lines[0].split(" ");
-
 let a = parseFloat(lines[0])
 let area =parseFloat(3.14159 * a*a);
 console.log("A="+ (area.toFixed(4)));
